c0db212d953286bbb86f13a658a7829c7baddd66
Ben Gosney <bengosney@googlemail.com>
2021-06-06 19:52:02 +0100

    Updates

---
+++ precache-manifest.3f8e883753f699ccfe691ec627c0ddeb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8292ad4b215f646d1ab0",
+    "url": "/boids/static/css/main.3202caf9.chunk.css"
+  },
+  {
+    "revision": "8292ad4b215f646d1ab0",
+    "url": "/boids/static/js/main.8c270076.chunk.js"
+  },
+  {
+    "revision": "dc4dd06ec1b3185874c3",
+    "url": "/boids/static/js/runtime~main.239e2317.js"
+  },
+  {
+    "revision": "fe9c95028f62cf811243",
+    "url": "/boids/static/js/2.6a7615e7.chunk.js"
+  },
+  {
+    "revision": "440af7a5b98d08432d91d9c5ea587931",
+    "url": "/boids/index.html"
+  }
+];
